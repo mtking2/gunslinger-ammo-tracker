c3a009da51983a3c1c1e88bd24b7876065138d54
Michael King <mking@hey.com>
2020-09-20 03:02:14 -0400

    Updates

---
+++ precache-manifest.6bcc092d9d909b3418d7109a4008bc42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64dee063a1a3a5b9f8e547cbdc3a8dbd",
+    "url": "/gunslinger-ammo-tracker/index.html"
+  },
+  {
+    "revision": "d9dfd382208cb911d8a7",
+    "url": "/gunslinger-ammo-tracker/static/css/main.7e141bea.chunk.css"
+  },
+  {
+    "revision": "c50959ec87fa90d8f6e2",
+    "url": "/gunslinger-ammo-tracker/static/js/2.507fcb27.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/gunslinger-ammo-tracker/static/js/2.507fcb27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9dfd382208cb911d8a7",
+    "url": "/gunslinger-ammo-tracker/static/js/main.638eda38.chunk.js"
+  },
+  {
+    "revision": "399125bd0c1e38c5fd8c",
+    "url": "/gunslinger-ammo-tracker/static/js/runtime-main.6302b8f7.js"
+  }
+]);
